a560e04e913f958083e2cd50627e04eaa226a7c7
Prince <prince@deriv.com>
2025-07-29 11:48:01 +0400

    chore: test shiftai worflows

---
+++ test.js
@@ -0,0 +1,22 @@
+// [AI]
+
+const malfunction  = () => {
+    a = 1;
+    b = 2;
+    c = a + b;
+    console.log(c);
+}
+
+malfunction();
+
+// [AI]
+
+const human = () => {
+    console.log("Human");
+    console.log("Human coded this");
+}
+
+human();
+
+
+
