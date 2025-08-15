89301eac7d9ac0b38fca327b7d173bbee9643cdd
Prince <prince@deriv.com>
2025-08-15 16:02:03 +0400

    chore: testing eslint on ai wrapper cleanup

--- packages/reports/src/index.tsx
+++ packages/reports/src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { makeLazyLoader } from '@deriv/shared';
+
 import { Loading } from '@deriv/components';
+import { makeLazyLoader } from '@deriv/shared';
+
 import 'promise-polyfill';
 
 const App = makeLazyLoader(
     () => import(/* webpackChunkName: "reports-app", webpackPreload: true */ './app'),
+
     () => <Loading />
 )();
 
