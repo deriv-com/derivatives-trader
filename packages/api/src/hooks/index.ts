abd0e06a652bc791ea3ec0eb5bca55412ec45c28
Akmal Djumakhodjaev <akmal@binary.com>
2025-09-10 14:35:14 +0800

    chore: remove unused export for useNotificationEvent from hooks index

--- packages/api/src/hooks/index.ts
+++ packages/api/src/hooks/index.ts
@@ -3,5 +3,4 @@ export { default as useFeatureFlags } from './useFeatureFlags';
 export { default as useIsRtl } from './useIsRtl';
 export { default as useIntercom, useIsIntercomAvailable } from './useIntercom';
 export { default as useLocalStorageData } from './useLocalStorageData';
-export { default as useNotificationEvent } from './useNotificationEvent';
 export { default as useAccountSettingsRedirect } from './useAccountSettingsRedirect';
