62928df5b3aa151adee2ebe16aa9b77086d76109
Nijil Nirmal <nijil@deriv.com>
2025-09-29 16:34:12 +0400

    fix:failing test cases

--- packages/trader/src/AppV2/Containers/Chart/__tests__/contract-details-chart.spec.tsx
+++ packages/trader/src/AppV2/Containers/Chart/__tests__/contract-details-chart.spec.tsx
@@ -24,11 +24,16 @@ jest.mock('AppV2/Hooks/useContractDetails', () => ({
         contract_info: {
             contract_id: 12345,
             contract_type: 'CALL',
-            underlying: 'R_50',
+            underlying_symbol: 'R_50',
         },
         contract: {},
         is_loading: false,
     }),
+}));
+
+jest.mock('Modules/Contract/Containers/replay-chart', () => ({
+    __esModule: true,
+    default: () => <div>Mocked Chart</div>,
 }));
 
 describe('Contract Replay Chart', () => {
@@ -39,7 +44,7 @@ describe('Contract Replay Chart', () => {
                     contract_info: {
                         contract_id: 12345,
                         contract_type: 'CALL',
-                        underlying: 'R_50',
+                        underlying_symbol: 'R_50',
                     },
                 },
             },
