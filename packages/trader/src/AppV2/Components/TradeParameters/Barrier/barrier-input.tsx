77ac7d84c31211f8ed8574e733c5cb8151b4f09e
Akmal Djumakhodjaev <akmal@binary.com>
2025-10-03 10:56:39 +0800

    feat: add useDebounce hook for improved input handling

--- packages/trader/src/AppV2/Components/TradeParameters/Barrier/barrier-input.tsx
+++ packages/trader/src/AppV2/Components/TradeParameters/Barrier/barrier-input.tsx
@@ -1,27 +1,11 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite';
 
+import { useDebounce } from '@deriv/api-v2';
 import { ActionSheet, Chip, Text, TextField, TextFieldAddon } from '@deriv-com/quill-ui';
 import { Localize, useTranslations } from '@deriv-com/translations';
 
 import { useTraderStore } from 'Stores/useTraderStores';
-
-// Debounce hook for real-time validation
-const useDebounce = (value: string, delay: number) => {
-    const [debouncedValue, setDebouncedValue] = React.useState(value);
-
-    React.useEffect(() => {
-        const handler = setTimeout(() => {
-            setDebouncedValue(value);
-        }, delay);
-
-        return () => {
-            clearTimeout(handler);
-        };
-    }, [value, delay]);
-
-    return debouncedValue;
-};
 
 const chips_options = [
     {
