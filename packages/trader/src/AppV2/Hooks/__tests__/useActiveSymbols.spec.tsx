1f31a67ee1cf38175321b70507a65072a76d11ec
Nijil Nirmal <nijil@deriv.com>
2025-10-22 12:24:21 +0400

    fix: test case failure

--- packages/trader/src/AppV2/Hooks/__tests__/useActiveSymbols.spec.tsx
+++ packages/trader/src/AppV2/Hooks/__tests__/useActiveSymbols.spec.tsx
@@ -149,7 +149,6 @@ describe('useActiveSymbols', () => {
                     contract_type: [CONTRACT_TYPES.VANILLA.CALL, CONTRACT_TYPES.VANILLA.PUT],
                 },
                 options: {
-                    enabled: true,
                     staleTime: 5 * 60 * 1000,
                     cacheTime: 10 * 60 * 1000,
                     refetchOnWindowFocus: false,
@@ -170,7 +169,6 @@ describe('useActiveSymbols', () => {
                     contract_type: [CONTRACT_TYPES.TURBOS.LONG, CONTRACT_TYPES.TURBOS.SHORT],
                 },
                 options: {
-                    enabled: true,
                     staleTime: 5 * 60 * 1000,
                     cacheTime: 10 * 60 * 1000,
                     refetchOnWindowFocus: false,
